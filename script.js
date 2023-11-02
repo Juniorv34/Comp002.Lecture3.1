e32f0d0ddddf8f6754163a3debe5ac6b382641d6
junior vargas <juniorvargascovarrub@my.whccd.edu>
2023-11-01 20:28:02 -0700

    I made an if code

---
+++ script.js
@@ -0,0 +1,13 @@
+let myGrade = 95;
+if (myGrade <= 99 && myGrade >= 90 ) {
+    console.log("A");
+} if(myGrade <= 89 && myGrade >= 80) {
+    console.log("B");
+} if(myGrade <= 79 && myGrade >= 70) {
+    console.log("C");
+} if(myGrade <= 69 && myGrade >= 60) {
+    console.log("D");
+} if(myGrade <= 59 && myGrade >= 0) {
+    console.log("F");
+}
+
